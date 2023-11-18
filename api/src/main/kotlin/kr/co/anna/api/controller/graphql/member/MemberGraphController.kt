0b2baf1f511f263213c551ae2689a6289e850b46
GunplaList9408 <9408commit@gmail.com>
2023-11-18 17:08:09 +0900

    feat: GraphQl 요청은 성공 했으나 값 return이 되지 않음

--- api/src/main/kotlin/kr/co/anna/api/controller/graphql/member/MemberGraphController.kt
+++ api/src/main/kotlin/kr/co/anna/api/controller/graphql/member/MemberGraphController.kt
@@ -1,17 +1,27 @@
 package kr.co.anna.api.controller.graphql.member
 
-import kr.co.anna.api.dto.user.UserOut
+import kr.co.anna.domain.model.user.User
 import kr.co.anna.domain.repository.user.UserRepository
 import org.springframework.graphql.data.method.annotation.QueryMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Controller
 
-@RestController
+/**
+ * Url: http://localhost:8080/graphql
+ * Https Method: POST
+ * Header: 'Content-Type : application/json'
+ * **/
+@Controller
 class MemberGraphController (
 
     private val userRepository: UserRepository
 
         ) {
 
+    /**
+    {
+        "query": "{ findAllUsers { oid userId name email } }"
+    }
+     **/
     @QueryMapping(value = "findAllUsers")
-    fun findAllUsers(): List<UserOut> = userRepository.findAll().map { UserOut.fromEntity(it) }
+    fun findAllUsers(): List<User> = userRepository.findAll()
 }
--- common/domain/src/main/kotlin/kr/co/anna/domain/repository/user/UserRepository.kt
+++ common/domain/src/main/kotlin/kr/co/anna/domain/repository/user/UserRepository.kt
@@ -3,5 +3,4 @@ package kr.co.anna.domain.repository.user
 import kr.co.anna.domain.model.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
-}
+interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom
